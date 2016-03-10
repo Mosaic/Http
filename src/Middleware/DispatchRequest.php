21d60eb5969d92b9debe4a14eb8bf9e49b8de43e
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-03-10 12:19:54 +0100

    Simplify the DispatchRequest middleware + pass the psr7 request to the dispatcher

--- src/Middleware/DispatchRequest.php
+++ src/Middleware/DispatchRequest.php
@@ -5,10 +5,7 @@ namespace Mosaic\Http\Middleware;
 use Mosaic\Http\Adapters\Psr7\Response;
 use Mosaic\Http\Request;
 use Mosaic\Http\ResponseFactory;
-use Mosaic\Routing\Dispatchers\DispatchClosure;
-use Mosaic\Routing\Dispatchers\DispatchController;
 use Mosaic\Routing\RouteDispatcher;
-use Mosaic\Routing\Router;
 
 class DispatchRequest
 {
@@ -23,41 +20,17 @@ class DispatchRequest
     private $dispatcher;
 
     /**
-     * @var Router
-     */
-    private $router;
-
-    /**
-     * @var DispatchClosure
-     */
-    private $closure;
-
-    /**
-     * @var DispatchController
-     */
-    private $controller;
-
-    /**
      * DispatchRequest constructor.
      *
-     * @param RouteDispatcher    $dispatcher
-     * @param Router             $router
-     * @param DispatchController $controller
-     * @param DispatchClosure    $closure
-     * @param ResponseFactory    $factory
+     * @param RouteDispatcher $dispatcher
+     * @param ResponseFactory $factory
      */
     public function __construct(
         RouteDispatcher $dispatcher,
-        Router $router,
-        DispatchController $controller,
-        DispatchClosure $closure,
         ResponseFactory $factory
     ) {
-        $this->router     = $router;
         $this->factory    = $factory;
         $this->dispatcher = $dispatcher;
-        $this->closure    = $closure;
-        $this->controller = $controller;
     }
 
     /**
@@ -69,14 +42,8 @@ class DispatchRequest
      */
     public function __invoke(Request $request)
     {
-        $route = $this->dispatcher->dispatch($request, $this->router->all());
-
-        if ($this->closure->isSatisfiedBy($route)) {
-            $response = $this->closure->dispatch($route);
-        } else {
-            $response = $this->controller->dispatch($route);
-        }
-
-        return $this->factory->make($response);
+        return $this->factory->make(
+            $this->dispatcher->dispatch($request->toPsr7())
+        );
     }
 }
